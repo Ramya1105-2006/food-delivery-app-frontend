82b4aa77676c6a13386ed7767ad041c30fdb9477
aura <mayflower1156@gmail.com>
2025-12-23 06:26:23 +0000

    Runtime Error
    Invalid src prop (http://3.bp.blogspot.com/-lPIFcRKn-rc/T

--- src/next.config.ts
+++ src/next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'media.istockphoto.com',
         port: '',
         pathname: '/**',
